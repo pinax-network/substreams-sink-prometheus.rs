b427ceca18f156225937d81c0e34ecb0a6133763
Denis Carriere <carriere.denis@gmail.com>
2023-02-05 22:14:01 -0500

    init prometheus server

---
+++ bin/server.ts
@@ -0,0 +1,22 @@
+import client from "prom-client";
+import http from "node:http";
+
+// Prometheus Exporter
+const register = new client.Registry();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register });
+
+// Create a local server to serve Prometheus gauges
+const server = http.createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': register.contentType });
+    res.end(await register.metrics());
+});
+
+export async function listen(port: number) {
+    return new Promise(resolve => {
+        server.listen(port, "0.0.0.0", () => {
+            console.log('starting prometheus metrics server', { port });
+            resolve(true);
+        });
+    })
+}
